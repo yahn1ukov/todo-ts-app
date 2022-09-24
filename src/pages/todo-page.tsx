9ebf2bfcd52331b669748c88ffb46afcfd05c12e
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2022-09-24 23:04:54 +0300

    feat: add signalr connect

--- src/pages/todo-page.tsx
+++ src/pages/todo-page.tsx
@@ -1,10 +1,46 @@
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { useContext, useEffect, useState } from "react";
 import ErrorBoundaries from "../components/error-boundaries/error-boundaries";
 import TodoCreateForm from "../components/todo-create-form/todo-create-form";
 import TodoFilterPanel from "../components/todo-filter-panel/todo-filter-panel";
 import TodoList from "../components/todo-list/todo-list";
-import { todo } from "../data/data";
+import AuthContext from "../context/auth.context";
+import { GetTodoResponse } from "../types/types";
 
 export const TodoPage = () => {
+    const { token } = useContext(AuthContext);
+    const [connection, setConnection] = useState<HubConnection|null>(null);
+    const [todo, setTodo] = useState<GetTodoResponse[]>([]);
+
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl("https://localhost:7066/hubs/todo", { 
+                accessTokenFactory: () => token as string
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(newConnection);
+    }, [token]);
+
+    useEffect(() => {
+        if(connection) {
+            console.log("Connected");
+            connection.start()
+                .then(() => {
+                    connection.on("LoadTodo", (todo: GetTodoResponse[]) => {
+                        setTodo(todo);
+                    })
+                })
+                .catch(error => console.log(error))
+        }
+
+        return () => {
+            console.log("Disconnected");
+            connection?.stop();
+        }
+    }, [connection]);
+
     return(
         <>
             <ErrorBoundaries>
